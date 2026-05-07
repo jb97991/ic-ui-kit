424bb9f7998b325246735f9dcb45705810e5f3a5
lz405 <179817187+lz405@users.noreply.github.com>
2026-05-07 14:36:43 +0000

    feat(web-components): add props to prevent closing of popover when options pressed
    add a prop on popover menu to prevent closing when any option or toggle options are pressed
    added a prop for individual menu options controlling how popover closing behaviour should be on
    individual menu options
    
    feat #4361
    
    feat(web-components): add props to prevent closing of popover when options pressed
    
    add a prop on popover menu to prevent closing when any option or toggle options are pressed
    added a prop for individual menu options controlling how popover closing behaviour should be on
    individual menu options
    
    feat #4361

--- packages/web-components/src/components/ic-popover-menu/ic-popover-menu.tsx
+++ packages/web-components/src/components/ic-popover-menu/ic-popover-menu.tsx
@@ -83,6 +83,11 @@ export class PopoverMenu {
   @Prop({ reflect: true, mutable: true }) open?: boolean | undefined =
     undefined;
 
+  /**
+   * If `always`, the popover menu will close when any menu item is clicked. If `toggle`, the popover menu will close when a non-toggle menu item is clicked. If `never`, the popover menu will not close when any menu item is clicked.
+   */
+  @Prop() closeOnItemSelect: "always" | "toggle" | "never" = "always";
+
   @Watch("open")
   watchOpenHandler(): void {
     const popoverArr = this.el.parentElement?.querySelectorAll(
@@ -166,6 +171,14 @@ export class PopoverMenu {
   @Listen("handleMenuItemClick")
   handleMenuItemClick(ev: CustomEvent<HTMLIcMenuItemElement>): void {
     if (!ev.detail.submenuTriggerFor && ev.detail.label !== "Back") {
+      if (
+        this.closeOnItemSelect === "never" ||
+        (ev.detail.variant === "toggle" &&
+          this.closeOnItemSelect === "toggle") ||
+        ev.detail.closeMenuOnClick === false
+      ) {
+        return;
+      }
       this.closeMenu(false, ev.detail);
     }
   }
